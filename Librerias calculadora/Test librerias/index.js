95d49cab46ccdb48c623b88fa6b5d6582f981290
alejandro06020 <alejandro.minda@epn.edu.ec>
2025-08-02 16:48:07 -0500

    Librerias calculadora

---
+++ Librerias calculadora/Test librerias/index.js
@@ -0,0 +1,7 @@
+const paqueteResta = require('2025a-swgr1-aama-resta');
+const paqueteDivision = require('2025a-swgr1-aama-division');
+
+const respuestaResta = paqueteResta.resta(4, 2);
+const respuestaDivision = paqueteDivision.division(4, 2);
+console.log('La respuesta es de la resta es: ', respuestaResta);
+console.log('La respuesta es de la division es: ', respuestaDivision);
